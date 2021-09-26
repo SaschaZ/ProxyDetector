f1dc641d3376610712955ffd9a0c641c96602d9d
Sascha Zieger <sascha@zieger.dev>
2021-09-27 00:44:15 +0200

    pretty print

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -9,11 +9,15 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.apache.log4j.BasicConfigurator
 
 private const val HOST = "http://ip-api.com/json/"
 private const val FIELDS =
     "?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
+
+private val json = Json { prettyPrint = true }
 
 private val client = HttpClient(OkHttp) {
     engine {
@@ -23,7 +27,7 @@ private val client = HttpClient(OkHttp) {
         }
     }
     install(JsonFeature) {
-        serializer = KotlinxSerializer()
+        serializer = KotlinxSerializer(json)
     }
 }
 
@@ -36,13 +40,14 @@ fun main() {
         routing {
             get("/") {
                 val ip = call.request.headers["X-Real-Ip"]
+
                 if (ip?.endsWith("localhost") != false) {
-                    call.respondText("localhost")
+                    call.respondText("no result")
                 } else {
                     val result = ipProxyMap[ip] ?: client.get("$HOST$ip$FIELDS")
                     ipProxyMap[ip] = result
 
-                    call.respondText("result for ip $ip: $result")
+                    call.respondText(json.encodeToString(result))
                 }
             }
         }
